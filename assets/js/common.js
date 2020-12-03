245928e48a4ad3d147a2aec8ff2fbacdb636de45
Sickenberg <haux49@gmail.com>
2020-12-03 20:54:14 +0100

    Notification for wip texts

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,3 +4,9 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 UIkit.use(Icons);
+UIkit.notification({
+    message: 'WIP Texts!',
+    timeout: null,
+    status: "warning",
+    pos: "top-center"
+})
