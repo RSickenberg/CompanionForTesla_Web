d5e72254c226d1d01e853c216204269f59cbc9f5
Sickenberg <haux49@gmail.com>
2020-12-04 16:09:52 +0100

    New images

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,9 +4,3 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 UIkit.use(Icons);
-UIkit.notification({
-    message: 'WIP Texts!',
-    timeout: null,
-    status: "warning",
-    pos: "top-center"
-})
