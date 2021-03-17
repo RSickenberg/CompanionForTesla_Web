8bc7ba1a36172003ac9411c713d0a57b7183cd79
Sickenberg Romain <haux49@gmail.com>
2021-03-17 14:35:11 +0100

    UIKIT update + warn message on CN issue

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,3 +4,6 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 UIkit.use(Icons);
+
+UIkit.notification('An issue has been found with CN account, it\'s not in my hand but a fix should come in the next weeks!',
+    {status: 'warning', pos: 'top-right'})
