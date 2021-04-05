3a89009ea133986e0aecc13ab3706b60fa3c20fc
Sickenberg Romain <haux49@gmail.com>
2021-04-05 16:40:09 +0200

    Removed warning message since it's fixed now.

--- assets/js/common.js
+++ assets/js/common.js
@@ -4,6 +4,3 @@ import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 UIkit.use(Icons);
-
-UIkit.notification('An issue has been found with CN account, it\'s not in my hand but a fix should come in the next weeks!',
-    {status: 'warning', pos: 'top-right'})
